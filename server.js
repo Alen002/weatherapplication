691f7c856acaf40b7652269e4ff3139776a7dbc0
Alen <alechkr@gmail.com>
2020-04-23 13:05:54 +0200

    client sends data to the POST route (=server)

--- server.js
+++ server.js
@@ -1,60 +1,55 @@
-// Require Express to run server and routes
+
+
+/* Express to run server and routes */
 const express = require('express');
 
-// Start up an instance of app
+/* Start up an instance of app */
 const app = express();
 
-// Cors for cross origin allowance
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-//Body-Parser for Post requests
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json());
-
-/** 
-Initialize the main project folder
-Connect server-side code in server.js to the client-side code stored in the website folder
-*/
+/* Initialize the main project folder*/
 app.use(express.static('website'));
-const port  = 5500;
 
-// Setup Server
-const server = app.listen (port, ()=>{
-    console.log(`running on localhost: ${port}`)
-});
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
 
-
-// Initialize an empty JS object named projectData
-const projectData = {}; 
-
-// Response with JS object when a GET request is made from the webpage (=client)
-app.get('/get', (req, res) => {
-    res.send(projectData);
-});
-
-//An HTTP POST request sends data to the project's endpoint, where it is stored and can be accessed through a GET request
-app.post('/add', (req, res) => {
-    projectData.temperature = req.body.temp;
-    projectData.date = req.body.date;
-    projectData.userResponse = req.body.userResponse;
-    projectData.push = 'fjkgjdkflghjdfkl'
-    res.send(projectData);
-});
-  
+// ****************** Server-Side Routes ****************** //
 
 
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
 
+app.get('/all', sendData);
 
+function sendData (request, response) {
+  response.send(projectData);
+};
 
+// TODO-ROUTES!
+/* app.post('/add', (request, response) => {
+    response.send('POST received'); 
+ });
 
+ */
 // POST an animal
 const data = [];
 
-app.post('/test', addAnimal);
-function addAnimal (req,res){
-    data.push(req.body = 'server.js');
-    res.send(data)
-};
+app.post('/add', (request, response) => {
+    
+    data.push(request.body); //receive the data from postData in app.js and make it actionable with request.body
+    response.send(data);
+});
+
 
--- website/app.js
+++ website/app.js
@@ -2,32 +2,31 @@
 //Put the credentials and base URL here
 
 // Weather URL -> The API key is retrieved from api.js
-const url = `https://api.openweathermap.org/data/2.5/weather?q=London&${API_key}`
-
+/* const url = `https://api.openweathermap.org/data/2.5/weather?q=London&${API_key}`
+const baseURL = 'http://localhost:5500';  */
 
 
 /* Function to POST data */
-let postData = async ( url = '', data = {})=>{ //something wrong here
-    const baseURL = 'http://localhost:5500'; 
+const postData = async ( url= '', data = {})=>{
     console.log(data)
-    
-      const response = await fetch(url, {   //url defined on linecode 10
-      method: 'POST', 
-      credentials: 'same-origin', 
+      const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      credentials: 'same-origin', // include, *same-origin, omit
       headers: {
           'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),     
+      body: JSON.stringify(data), // body data type must match "Content-Type" header        
     });
   
       try {
         const newData = await response.json();
-        console.log(newData);
-        return newData;
+        // console.log(newData);
+        return newData
       }catch(error) {
       console.log("error", error);
+      // appropriately handle the error
       }
   }
 
-  postData('/test', {animal:'app.js'}) //not working
-
+  
+  postData('/add', {animal:'lion'}) // '/add' is passing in the URL of the POST route on server.js
